e0c8762a3ecf8eef828ae75fe10eecd13cb0e83d
Brian Lee <Brian Lee>
2017-04-03 20:45:24 -0500

    Third Phase Overflow number hidden

--- script/base.js
+++ script/base.js
@@ -1,7 +1,7 @@
 $(function() {
 var cbtn = $(".calcbutton");
 var output = $("#digits");
-//var calRes = ";
+var calRes = "";
 console.log(cbtn);
 
 //Use loop to create individual class buttons that equivalent
@@ -9,7 +9,10 @@ for(var i=0; i < cbtn.length; i++) {
 
     $(cbtn[i]).click( function(){ //cbtn array is re-wrapped in Jquery $ scope
         var clkvalue = $(this).text();
-        output.text(clkvalue); 
+       //output.text(clkvalue);
+
+        calRes=calRes + clkvalue;
+        output.text(calRes); 
         console.log("Clicked!");
     });
 }
