86d74e546b3c7d4923f7265b720cc2c371453a10
Brian <tzhlee@gmail.com>
2017-04-05 22:13:52 -0500

    0 integer issue solved

--- script/base.js
+++ script/base.js
@@ -2,6 +2,8 @@ $(function() {
 var cbtn = $(".calcbutton");
 var output = $("#digits");
 var calRes = "";
+var boolswitch = false; //Use boolean as switch mechanism for controlling 0
+var boolopswitch = false; //Use bool as switch for continous calculation for operators without = sign 
 console.log(cbtn);
 
 //Use loop to create individual class buttons that equivalent
@@ -10,9 +12,19 @@ for(var i=0; i < cbtn.length; i++) {
     $(cbtn[i]).click( function(){ //cbtn array is re-wrapped in Jquery $ scope
         var clkvalue = $(this).text();
        //output.text(clkvalue);
+       
+       if(boolswitch || calRes == "0") { //Clears out results blank to restart entry of calculation & prevent 0 at start as first number 
+           boolswitch = false;
+           calRes = "";
+       }
 
        if(clkvalue == "=") {
         calRes = eval(calRes); //Eval NOT recommended! but essential for this version of the project
+        boolswitch = true; //Act as confirmation calculation end
+       }
+       else if (clkvalue == "C") {
+        calRes = 0;
+        boolswitch = true; //Assure that when clear is hit, then 0 is displayed as restart
        }
        else {
         calRes=calRes + clkvalue;
